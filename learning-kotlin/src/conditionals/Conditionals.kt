29678e67a08ab51e44d188b756ade553f93f539f
Connor Prinster <connor.prinster@gmail.com>
2019-06-07 15:28:58 -0600

    very small update thing

--- learning-kotlin/src/conditionals/Conditionals.kt
+++ learning-kotlin/src/conditionals/Conditionals.kt
@@ -3,37 +3,41 @@ package conditionals
 import java.util.*
 
 fun main(args: Array<String>) {
-//    val age:Int = 1
-//    if(age < 15) {
-//        print("You cannot register. You are only $age")
-//    }
-//    else if (age >= 15 && age <21) {
-//        print("Must register as younger than an adult as you are $age")
-//    } else {
-//        print("No restrictions for you as you are $age")
-//    }
-
-//    val catch : Int = 9
-//    val result : String = when(catch) {
-//        1 -> {
-//            "mode $catch"
-//        }
-//        2 -> {
-//            "mode $catch"
-//            "what's interesting is that"
-//            "nothing above this will be returned"
-//            "only this line will be assigned to 'result"
-//        }
-//        else -> {
-//            "mode $catch"
-//        }
-//    }
-
-//    val range : Int = 3
-//    if(range in 1..10) {
-//        print("hello")
-//    }
+    normalConditionals()
     challenge()
+}
+
+fun normalConditionals() {
+    val age:Int = 1
+    if(age < 15) {
+        print("You cannot register. You are only $age")
+    }
+    else if (age >= 15 && age <21) {
+        print("Must register as younger than an adult as you are $age")
+    } else {
+        print("No restrictions for you as you are $age")
+    }
+
+    val catch : Int = 9
+    val result : String = when(catch) {
+        1 -> {
+            "mode $catch"
+        }
+        2 -> {
+            "mode $catch"
+            "what's interesting is that"
+            "nothing above this will be returned"
+            "only this line will be assigned to 'result"
+        }
+        else -> {
+            "mode $catch"
+        }
+    }
+
+    val range : Int = 3
+    if(range in 1..10) {
+        print("hello")
+    }
 }
 
 fun challenge() {
