8d472c15bcbc4532728908e2a23c515bf6ecaedb
Connor Prinster <connor.prinster@gmail.com>
2019-07-29 23:20:39 -0600

    work on the xml
    editing stuff still
    
    added the text that changes based on the clicking
    
    more work
    
    can now tell you how old you are
    
    got another case where the DoB is in the future will caues an error
    
    working on the reformatting
    
    centered things better
    
    got a text size to a better value
    
    added a toast
    
    added some errors to check for invalid inputs
    
    added some placeholder text

--- learning-android/findMyAge/app/src/main/java/com/prinster/findmyage/MainActivity.kt
+++ learning-android/findMyAge/app/src/main/java/com/prinster/findmyage/MainActivity.kt
@@ -2,11 +2,39 @@ package com.prinster.findmyage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+  var cal = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+
+      goButton.setOnClickListener {
+        val inputYear = enterDob.text.toString()
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        if(inputYear.isEmpty()) {
+          Toast.makeText(
+            applicationContext,
+            R.string.empty_input,
+            Toast.LENGTH_SHORT
+          ).show()
+        } else if(Integer.parseInt(inputYear) > currentYear) {
+          Toast.makeText(
+            applicationContext,
+            R.string.future_error,
+            Toast.LENGTH_SHORT
+          ).show()
+        } else {
+          ageResult.text =
+            getString(
+              R.string.result_text,
+              (currentYear - Integer.parseInt(inputYear)).toString())
+        }
+      }
     }
 }
