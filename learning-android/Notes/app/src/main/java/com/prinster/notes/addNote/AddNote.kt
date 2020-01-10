4c4d418b3448de8bfdf5a79ad456f252c9716973
Connor Prinster <connor.prinster@gmail.com>
2020-01-10 11:18:18 -0700

    adding an add note activity

---
+++ learning-android/Notes/app/src/main/java/com/prinster/notes/addNote/AddNote.kt
@@ -0,0 +1,13 @@
+package com.prinster.notes.addNote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prinster.notes.R
+
+class AddNote : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_note)
+    }
+}
--- learning-android/Notes/app/src/main/java/com/prinster/notes/note/Note.kt
+++ learning-android/Notes/app/src/main/java/com/prinster/notes/note/Note.kt
@@ -1,11 +1,32 @@
 package com.prinster.notes.note
 
-class Note(val title: String, val content: String) {
+class Note {
+
+    private var title: String = ""
+    private var content: String = ""
+    private var id: String = ""
+
+    constructor(title: String, content: String) {
+        this.title = title
+        this.content = content
+        this.id = ""
+    }
+
+    constructor(title: String, content: String, id: String) {
+        this.title = title
+        this.content = content
+        this.id = id
+    }
+
     fun returnTitle(): String {
         return title
     }
 
     fun returnContent(): String {
         return content
+    }
+
+    fun returnID(): String {
+        return id
     }
 }
