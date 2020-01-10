8436c462fcee76e9c00eb8d50318884a4dfab9a1
Connor Prinster <connor.prinster@gmail.com>
2020-01-10 15:11:27 -0700

    added menu buttons and can add a new nonpersistent note

--- learning-android/Notes/app/src/main/java/com/prinster/notes/MainActivity.kt
+++ learning-android/Notes/app/src/main/java/com/prinster/notes/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.prinster.notes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.prinster.notes.adapter.NoteListAdapter
+import com.prinster.notes.addNote.AddNote
 import com.prinster.notes.note.Note
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,6 +26,26 @@ class MainActivity : AppCompatActivity() {
         note_list_rv.adapter = NoteListAdapter(notes, this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.add_note_menu -> {
+                val addNoteIntent = Intent(this, AddNote::class.java)
+                startActivity(addNoteIntent)
+            }
+            R.id.search_note_menu -> {
+                makeClickedToast("search note menu")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun createNotes() {
         notes = arrayListOf(
             Note("title0", "string"),
@@ -30,5 +55,9 @@ class MainActivity : AppCompatActivity() {
             Note("title4", "string"),
             Note("title5", "string")
         )
+    }
+
+    fun makeClickedToast(message: String) {
+        Toast.makeText(this, "You clicked on $message", Toast.LENGTH_SHORT).show()
     }
 }
