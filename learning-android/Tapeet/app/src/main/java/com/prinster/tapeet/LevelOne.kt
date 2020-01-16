ecb52eaa87280ceee6fb4fff50d407545d888ab6
Connor Prinster <connor.prinster@gmail.com>
2020-01-15 18:16:59 -0700

    more work

---
+++ learning-android/Tapeet/app/src/main/java/com/prinster/tapeet/LevelOne.kt
@@ -0,0 +1,12 @@
+package com.prinster.tapeet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_one)
+    }
+}
